80a68432e690cd254a0219d9aa0bae6461d58c44
tostos <saittosun2@gmail.com>
2021-03-22 06:45:22 +0100

    customers work

--- src/app/customers/pages/overview/customers.component.ts
+++ src/app/customers/pages/overview/customers.component.ts
@@ -1,3 +1,4 @@
+import { CustomerFacade } from './../../services/customer.facade';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subject } from 'rxjs';
 import { CustomerService } from '~customers/services/customer.service';
@@ -9,12 +10,26 @@ import { Customer } from '~types/customer';
   styleUrls: ['./customers.component.scss']
 })
 export class CustomersOverviewPageComponent implements OnInit, OnDestroy {
-  customers: Customer[] = [];
+  // customers: Customer[] = [];
+  customers: Customer[];
+  filteredCustomers: Customer[]
   private destroyed$ = new Subject<boolean>();
 
-  constructor(private customerService: CustomerService) { }
+  constructor(private customerService: CustomerService,
+              private store: CustomerFacade) { }
 
   ngOnInit(): void {
+    this.store.getCustomers().subscribe(customers => {
+      if(customers.length === 0) {
+        this.customerService.fetchCustomers().subscribe(customers => {
+          this.store.setCustomers(customers);
+          console.log(customers);
+          this.customers = customers;
+        });
+      } else {
+        this.customers = customers;
+      }
+    })
   }
 
   ngOnDestroy(): void {
